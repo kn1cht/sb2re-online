4627bdfb020e0c9b987c31a9834321263b1cf6a5
kn1cht <16277200+kn1cht@users.noreply.github.com>
2022-06-08 19:41:58 +0900

    initlal commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Online ScrapBox to Re:VIEW Converter
+
+This tiny web app converts [scrapbox](http://scrapbox.io) format to [Re:VIEW](https://github.com/kmuto/review) format.
+
+## Development
+- install [deno](https://deno.land/)
+- install [packup](https://github.com/kt3k/packup)
+- `packup index.html`
+
+## LICENSE
+[MIT License Copyright (c) 2022 kn1cht](./LICENSE)
+
+This app's design is based on [md2sb-online by hashrock](https://github.com/hashrock/md2sb-online).
+
+[MIT License Copyright (c) 2022 hashrock](https://github.com/hashrock/md2sb-online/blob/master/LICENSE)
---
+++ app.tsx
@@ -0,0 +1,35 @@
+// LICENSE : MIT
+
+import React from "https://cdn.skypack.dev/react@18.1.0?dts";
+import ReactDOM from "https://cdn.skypack.dev/react-dom@18.1.0?dts";
+import { useForm } from "https://cdn.skypack.dev/react-hook-form?dts";
+import scrapboxToReView from "https://raw.githubusercontent.com/fabon-f/sb2re/master/mod.ts";
+
+const sampleTxt = `サンプル
+[*** Online Scrapbox to Re:VIEW Converter]
+ [* Scrapbox 記法]から [* Re:VIEW 記法]へ変換します
+ \`sb2re\`を使用しています
+  [fabon-f/sb2re https://github.com/fabon-f/sb2re]
+`;
+
+function App() {
+  const { register, watch } = useForm();
+  const watchInput = watch("input", sampleTxt);
+  return (
+    <div id="app">
+      <div class="nav">
+        <h1>sb2re-online: Online Scrapbox to Re:VIEW Converter</h1>
+      </div>
+      <div class="editor">
+        <div class="editor_wrapper">
+          <textarea value={watchInput} class="input" {...register("input")}></textarea>
+        </div>
+        <div class="editor_wrapper">
+          <textarea value={scrapboxToReView(watchInput.trim())} class="output"></textarea>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+addEventListener("DOMContentLoaded", () => ReactDOM.render(<App />, document.querySelector("#main")));
